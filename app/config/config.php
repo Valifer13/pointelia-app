c49a0e68c6e0512b5b5ee52e352bce100fc40de1
Valifer13 <vianviszello@gmail.com>
2026-02-03 08:02:25 +0800

    change base url

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,4 @@ define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "pointeliadb");
 define("DB_PORT", "3306");
-define("BASE_URL", "http://localhost/poin-violation-app");
+define("BASE_URL", $_SERVER['HTTP_HOST'] === 'localhost' ? $_SERVER['HTTP_HOST'] . '/localhost' : $_SERVER['HTTP_HOST']);
